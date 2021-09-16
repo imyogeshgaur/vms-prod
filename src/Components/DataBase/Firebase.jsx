f9a8ea8c8c3a2ecaad811afaca772670be08a62b
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-09-16 20:09:08 +0530

    initial build done

--- src/Components/DataBase/Firebase.jsx
+++ src/Components/DataBase/Firebase.jsx
@@ -7,7 +7,7 @@ const firebaseConfig = {
   projectId: "vistor-management-system-1c98c",
   storageBucket: "vistor-management-system-1c98c.appspot.com",
   messagingSenderId: "492966676663",
-  appId: "1:492966676663:web:2f51a34a0064c1d9632532"
+  appId: "1:492966676663:web:5bef6c59b220952d632532"
 };
 
 const app = firebase.initializeApp(firebaseConfig);
